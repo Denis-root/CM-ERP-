8d8c292ac072af3afa470dc030fdbe1775c94952
Denis Bolaines <dgueber@gmail.com>
2024-09-05 01:54:23 -0600

    Se agrega plan de trabajo e instrucciones

--- sa@DockMsql2022.sql
+++ sa@DockMsql2022.sql
@@ -104,15 +104,29 @@ GO
 INSERT INTO CATEGORIAEQUIPO (codigo, categoria) VALUES
 ('CAT100', 'Herramientas Eléctricas'),
 ('CAT200', 'Herramientas Manuales'),
-('CAT300', 'Equipos de Medición');
+('CAT300', 'Equipos de Medición')
+('CAT400', 'Equipos de Seguridad'),
+('CAT500', 'Accesorios Generales');
+
 GO
 INSERT INTO CLIENTE (codigo, nombres, apellido, correo, telefono) VALUES
 ('CLI100', 'Juan', 'Pérez', 'juan.perez@email.com', '555-1234'),
-('CLI200', 'María', 'López', 'maria.lopez@email.com', '555-5678');
+('CLI200', 'María', 'López', 'maria.lopez@email.com', '555-5678'),
+('CLI300', 'Carlos', 'Martínez', 'carlos.martinez@email.com', '555-9101'),
+('CLI400', 'Ana', 'Ramírez', 'ana.ramirez@email.com', '555-1213'),
+('CLI500', 'Jesus', 'Soriano', 'jesus.sorianoz@email.com', '555-3244');
+
+
 GO
 INSERT INTO EQUIPO (idcategoria, codigo, nombre, modelo, marca, serie, estado) VALUES
 (1, 'EQ100', 'Taladro', 'T-800', 'Black&Decker', 'BD12345', 1),
-(2, 'EQ200', 'Martillo', 'M-900', 'Stanley', 'ST67890', 1);
+(2, 'EQ200', 'Martillo', 'M-900', 'Stanley', 'ST67890', 1),
+(3, 'EQ300', 'Sierra Circular', 'SC-550', 'DeWalt', 'DW2020', 1),
+(1, 'EQ400', 'Destornillador Eléctrico', 'DE-400', 'Makita', 'MK4499', 1),
+(2, 'EQ500', 'Lijadora', 'L-300', 'Bosch', 'BS7890', 1);
+
+
+
 GO
 INSERT INTO MECANICO (codigo, nombres, apellido, correo, telefono, tarifahora) VALUES
 ('MEC001', 'Luis', 'Alvarez', 'luis.alvarez@example.com', '300-400-5000', 75.00),
@@ -121,14 +135,23 @@ GO
 INSERT INTO PRODUCTO (codigo, producto, precio) VALUES
 ('PROD001', 'Aceite de motor 5W-30', 120.00),
 ('PROD002', 'Filtro de aire', 50.00);
+
 GO
 INSERT INTO ODSCAB (idcliente, idmecanico, fecha) VALUES
 (1, 1, '2024-01-15'),
-(2, 2, '2024-01-16');
+(2, 2, '2024-01-16')
+(1, 2, '2024-01-19'),
+(2, 1, '2024-01-20'),
+(1, 1, '2024-01-21');
+
+
 GO
 INSERT INTO ODSET (idodscab, idproducto, codigo, cantidad, precio) VALUES
 (1, 1, 'PROD1', 5, 150.00),
-(2, 1, 'PROD2', 3, 75.00);
+(2, 1, 'PROD2', 3, 75.00)
+(4, 2, 'PROD2', 2, 50.00),
+(5, 1, 'PROD1', 1, 120.00)
+(3, 2, 'PROD2', 4, 75.00); 
 
 
 
